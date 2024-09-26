68249f4d8a30dd9edcb97f69fd87c05c8435c47f
ntthanh1976 <49704522+ntthanh1976@users.noreply.github.com>
2024-09-26 13:13:12 +0700

    question

---
+++ src/main/java/model/QLKhachHang.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.util.ArrayList;
+import util.FileHelper;
+/*
+  Tác giả: Họ tên sinh viên
+*/
+public class QLKhachHang {
+
+    private ArrayList<KhachHang> dsKhachHang;
+
+    public QLKhachHang() {
+        dsKhachHang = new ArrayList<>();
+    }
+
+    public QLKhachHang(ArrayList<KhachHang> dsKhachHang) {
+        this.dsKhachHang = dsKhachHang;
+    }
+
+    public ArrayList<KhachHang> getDsKhachHang() {
+        return dsKhachHang;
+    }
+
+    public void setDsKhachHang(ArrayList<KhachHang> dsKhachHang) {
+        this.dsKhachHang = dsKhachHang;
+    }
+
+    //sinh viên cải đặt cho các phương thức xử lý sau
+   
+    public void DocKhachHang(String filename) {
+        
+    }
+
+    public boolean GhiHoaDon(String filename) {
+        
+       return true;
+    }
+   
+    public void sapXepTheoLoaiHinh() {
+               
+    }
+    
+    public double getTieuThuCaoNhat()
+    {
+      return 0;    
+    }
+    
+    public double getTieuThuThapNhat()
+    {
+       return 0;  
+    }
+    
+    public double getTieuThuTrungBinh()
+    {
+      return 0;   
+    }
+}
